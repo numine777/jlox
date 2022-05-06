925135d4f94ea2605c8b7b31656dc45259afa002
Scott Walls <scottwalls2008@gmail.com>
2022-05-06 14:43:11 -0500

    Add nix flake definition

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development environment for jlox";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        sys_pkgs = { aarch64-darwin = "x86_64-darwin"; x86_64-darwin = "x86_64-darwin"; x86_64-linux = "x86_64-linux"; };
+        sys_name = sys_pkgs.${system};
+        pkgs = nixpkgs.legacyPackages.${sys_name};
+        packageName = "jlox";
+      in
+      {
+        devShell = (pkgs.buildFHSUserEnv {
+          name = "jlox-dev-env";
+          targetPkgs = pkgs: (with pkgs; [
+            bazel_5
+            git
+            glibc
+            gcc
+            jdk11
+          ]);
+        }).env;
+      });
+}
