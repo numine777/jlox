7163071522ad09c2c990725775ab6bba796c6051
Scott Walls <scottwalls2008@gmail.com>
2022-05-14 15:52:52 -0500

    CH9 No challenges

--- src/main/java/com/numine777/lox/AstPrinter.java
+++ src/main/java/com/numine777/lox/AstPrinter.java
@@ -23,6 +23,11 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
     public String visitUnaryExpr(Expr.Unary expr) {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
@@ -58,16 +63,5 @@ class AstPrinter implements Expr.Visitor<String> {
         builder.append(")");
 
         return builder.toString();
-    }
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
-                new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(new Expr.Literal(45.67)));
-
-        System.out.println(new AstPrinter().print(expression));
     }
 }
